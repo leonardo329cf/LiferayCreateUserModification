27e287422d895172fa685481193b5d9d598bd158
leonardo <leonardo.ferreira@fakeMail.com>
2021-09-03 09:26:03 -0300

    ScreenNameLocalFormatter created and base impl copied from liferay, NewScreenNameValidator implemented

---
+++ modules/ScreenNameLocalFormatter/src/main/java/com/liferay/formatter/keys/NewFormatterKeys.java
@@ -0,0 +1,7 @@
+package com.liferay.formatter.keys;
+
+public interface NewFormatterKeys {
+
+	public static final String USERS_SCREEN_NAME_COMPANY_EMAIL =
+			"users.screen.name.company.email";
+}
---
+++ modules/ScreenNameLocalFormatter/src/main/java/com/liferay/portal/security/pwd/PwdToolkitUtilThreadLocal.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.pwd;
+
+import com.liferay.petra.lang.CentralizedThreadLocal;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class PwdToolkitUtilThreadLocal {
+
+	public static boolean isValidate() {
+		return _validate.get();
+	}
+
+	public static void setValidate(boolean validate) {
+		_validate.set(validate);
+	}
+
+	private static final ThreadLocal<Boolean> _validate =
+		new CentralizedThreadLocal<>(
+			PwdToolkitUtilThreadLocal.class + "._validate", () -> Boolean.TRUE,
+			false);
+
+}
