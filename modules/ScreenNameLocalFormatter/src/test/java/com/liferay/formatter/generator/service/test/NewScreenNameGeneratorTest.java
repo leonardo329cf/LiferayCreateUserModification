f53c9a3fc279a68d065113df0dbbb015d578100e
leonardo <leonardo.ferreira@fakeMail.com>
2021-09-16 10:27:29 -0300

    Refactoring NewScreenNameGenerator to use @Reference insted of <className>+Util, related tests updated

--- modules/ScreenNameLocalFormatter/src/test/java/com/liferay/formatter/generator/service/test/NewScreenNameGeneratorTest.java
+++ modules/ScreenNameLocalFormatter/src/test/java/com/liferay/formatter/generator/service/test/NewScreenNameGeneratorTest.java
@@ -19,21 +19,19 @@ import com.liferay.formatter.keys.NewFormatterKeys;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.GroupWrapper;
 import com.liferay.portal.kernel.model.UserWrapper;
-import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
-import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.PrefsProps;
+import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.PropsUtil;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.mockito.AdditionalMatchers;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -43,23 +41,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class NewScreenNameGeneratorTest {
 
-	@After
-	public void close() {
-		_propsUtil.close();
-		_prefsPropsUtil.close();
-		_userLocalServiceUtil.close();
-		_groupLocalServiceUtil.close();
-	}
-
-	@Before
-	public void init() {
-		_propsUtil = Mockito.mockStatic(PropsUtil.class);
-		_prefsPropsUtil = Mockito.mockStatic(PrefsPropsUtil.class);
-		_userLocalServiceUtil = Mockito.mockStatic(UserLocalServiceUtil.class);
-		_groupLocalServiceUtil = Mockito.mockStatic(
-			GroupLocalServiceUtil.class);
-	}
-
 	@Test
 	public void testGenerate_AddsEmailSufix_When_ScreenNameIsUnusedByOtherUserOrGroup_And_ScreenNameDoesNotContainsReservedWords()
 		throws Exception {
@@ -68,46 +49,42 @@ public class NewScreenNameGeneratorTest {
 
 		String input = "user@liferay.com";
 		String expected = "user" + _correctEmail;
-		_propsUtil.when(
-			() -> PropsUtil.get(
-				NewFormatterKeys.USERS_SCREEN_NAME_COMPANY_EMAIL)
+		Mockito.when(
+			_props.get(NewFormatterKeys.USERS_SCREEN_NAME_COMPANY_EMAIL)
 		).thenReturn(
 			_correctEmail
 		);
-		_propsUtil.when(
-			() -> PropsUtil.get(PropsKeys.ADMIN_RESERVED_SCREEN_NAMES)
+		Mockito.when(
+			_props.get(PropsKeys.ADMIN_RESERVED_SCREEN_NAMES)
 		).thenReturn(
 			_adminReservedScreenNames
 		);
 
-		_prefsPropsUtil.when(
-			() -> PrefsPropsUtil.getStringArray(
+		Mockito.when(
+			_prefsProps.getStringArray(
 				Mockito.anyLong(), Mockito.anyString(), Mockito.anyString(),
 				Mockito.any())
 		).thenReturn(
 			_adminReservedScreenNamesArray
 		);
 
-		_userLocalServiceUtil.when(
-			() -> UserLocalServiceUtil.fetchUserByScreenName(
+		Mockito.when(
+			_userLocalService.fetchUserByScreenName(
 				Mockito.anyLong(), Mockito.anyString())
 		).thenReturn(
 			null
 		);
 
-		_groupLocalServiceUtil.when(
-			() -> GroupLocalServiceUtil.fetchFriendlyURLGroup(
+		Mockito.when(
+			_groupLocalService.fetchFriendlyURLGroup(
 				Mockito.anyLong(), Mockito.anyString())
 		).thenReturn(
 			null
 		);
-
-		NewScreenNameGenerator newScreenNameGenerator =
-			new NewScreenNameGenerator();
 
 		// Act
 
-		String result = newScreenNameGenerator.generate(0L, 0L, input);
+		String result = _newScreenNameGenerator.generate(0L, 0L, input);
 
 		// Assert
 
@@ -115,7 +92,7 @@ public class NewScreenNameGeneratorTest {
 	}
 
 	@Test
-	public void testGenerate_CreatesNewScreenName_When_ScreenNameIsAlreadyUseByGroup()
+	public void testGenerate_CreatesNewScreenName_When_ScreenNameIsAlreadyInUseByGroup()
 		throws Exception {
 
 		// Arrange
@@ -124,53 +101,43 @@ public class NewScreenNameGeneratorTest {
 		String prefix = "user";
 		String expected = "user1" + _correctEmail;
 
-		_propsUtil.when(
-			() -> PropsUtil.get(
-				NewFormatterKeys.USERS_SCREEN_NAME_COMPANY_EMAIL)
+		Mockito.when(
+			_props.get(NewFormatterKeys.USERS_SCREEN_NAME_COMPANY_EMAIL)
 		).thenReturn(
 			_correctEmail
 		);
-		_propsUtil.when(
-			() -> PropsUtil.get(PropsKeys.ADMIN_RESERVED_SCREEN_NAMES)
+		Mockito.when(
+			_props.get(PropsKeys.ADMIN_RESERVED_SCREEN_NAMES)
 		).thenReturn(
 			_adminReservedScreenNames
 		);
 
-		_prefsPropsUtil.when(
-			() -> PrefsPropsUtil.getStringArray(
+		Mockito.when(
+			_prefsProps.getStringArray(
 				Mockito.anyLong(), Mockito.anyString(), Mockito.anyString(),
 				Mockito.any())
 		).thenReturn(
 			_adminReservedScreenNamesArray
 		);
 
-		_userLocalServiceUtil.when(
-			() -> UserLocalServiceUtil.fetchUserByScreenName(
+		Mockito.when(
+			_userLocalService.fetchUserByScreenName(
 				Mockito.anyLong(), Mockito.anyString())
 		).thenReturn(
 			null
 		);
 
-		_groupLocalServiceUtil.when(
-			() -> GroupLocalServiceUtil.fetchFriendlyURLGroup(
+		Mockito.when(
+			_groupLocalService.fetchFriendlyURLGroup(
 				Mockito.anyLong(),
 				Mockito.eq(StringPool.SLASH + prefix + _correctEmail))
 		).thenReturn(
 			_groupWrapperMock
 		);
-		_groupLocalServiceUtil.when(
-			() -> GroupLocalServiceUtil.fetchFriendlyURLGroup(
-				Mockito.anyLong(), Mockito.eq(expected))
-		).thenReturn(
-			null
-		);
-
-		NewScreenNameGenerator newScreenNameGenerator =
-			new NewScreenNameGenerator();
 
 		// Act
 
-		String result = newScreenNameGenerator.generate(0L, 0L, input);
+		String result = _newScreenNameGenerator.generate(0L, 0L, input);
 
 		// Assert
 
@@ -189,73 +156,79 @@ public class NewScreenNameGeneratorTest {
 
 		String expected = prefix + "1" + _correctEmail;
 
-		_propsUtil.when(
-			() -> PropsUtil.get(
-				NewFormatterKeys.USERS_SCREEN_NAME_COMPANY_EMAIL)
+		Mockito.when(
+			_props.get(NewFormatterKeys.USERS_SCREEN_NAME_COMPANY_EMAIL)
 		).thenReturn(
 			_correctEmail
 		);
-		_propsUtil.when(
-			() -> PropsUtil.get(PropsKeys.ADMIN_RESERVED_SCREEN_NAMES)
+		Mockito.when(
+			_props.get(PropsKeys.ADMIN_RESERVED_SCREEN_NAMES)
 		).thenReturn(
 			_adminReservedScreenNames
 		);
 
-		_prefsPropsUtil.when(
-			() -> PrefsPropsUtil.getStringArray(
+		Mockito.when(
+			_prefsProps.getStringArray(
 				Mockito.anyLong(), Mockito.anyString(), Mockito.anyString(),
 				Mockito.any())
 		).thenReturn(
 			_adminReservedScreenNamesArray
 		);
 
-		_userLocalServiceUtil.when(
-			() -> UserLocalServiceUtil.fetchUserByScreenName(
+		Mockito.when(
+			_userLocalService.fetchUserByScreenName(
 				Mockito.anyLong(), Mockito.eq(prefix + _correctEmail))
 		).thenReturn(
 			_userWrapperMock
 		);
 
-		_userLocalServiceUtil.when(
-			() -> UserLocalServiceUtil.fetchUserByScreenName(
+		Mockito.when(
+			_userLocalService.fetchUserByScreenName(
 				Mockito.anyLong(),
 				AdditionalMatchers.not(Mockito.eq(prefix + _correctEmail)))
 		).thenReturn(
 			null
 		);
 
-		_groupLocalServiceUtil.when(
-			() -> GroupLocalServiceUtil.fetchFriendlyURLGroup(
+		Mockito.when(
+			_groupLocalService.fetchFriendlyURLGroup(
 				Mockito.anyLong(), Mockito.anyString())
 		).thenReturn(
 			null
 		);
 
-		NewScreenNameGenerator newScreenNameGenerator =
-			new NewScreenNameGenerator();
-
 		// Act
 
-		String result = newScreenNameGenerator.generate(0L, 0L, input);
+		String result = _newScreenNameGenerator.generate(0L, 0L, input);
 
 		// Assert
 
 		Assert.assertEquals(expected, result);
 	}
 
-	private String _adminReservedScreenNames = "ADMIN\nAdmin\nadmin";
-	private String[] _adminReservedScreenNamesArray = {
+	private final String _adminReservedScreenNames = "ADMIN\nAdmin\nadmin";
+	private final String[] _adminReservedScreenNamesArray = {
 		"ADMIN", "Admin", "admin"
 	};
-	private String _correctEmail = "@company.com";
-	private MockedStatic<GroupLocalServiceUtil> _groupLocalServiceUtil;
+	private final String _correctEmail = "@company.com";
+
+	@Mock
+	private GroupLocalService _groupLocalService;
 
 	@Mock
 	private GroupWrapper _groupWrapperMock;
 
-	private MockedStatic<PrefsPropsUtil> _prefsPropsUtil;
-	private MockedStatic<PropsUtil> _propsUtil;
-	private MockedStatic<UserLocalServiceUtil> _userLocalServiceUtil;
+	@InjectMocks
+	private NewScreenNameGenerator _newScreenNameGenerator;
+
+	@Mock
+	private PrefsProps _prefsProps;
+
+	@Mock
+	private Props _props;
+
+	@Mock
+	private UserLocalService _userLocalService;
 
 	@Mock
 	private UserWrapper _userWrapperMock;
